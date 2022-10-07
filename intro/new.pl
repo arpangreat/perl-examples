8dfbc88fb66c49a5ceafbbd7aa720ec4ca6b902c
arpangreat <arpanthegreat41@gmail.com>
2022-10-07 18:51:13 +0530

    first commit

---
+++ intro/new.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+print "Hello, Swastik!\n";
+print "How are you?\n"
---
+++ strings/strings.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $name = "Swastik";
+
+print "Hello, $name\n"
